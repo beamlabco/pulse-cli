148ca3e3dafa4ee5883bdc2f57a78f14c62724a8
Avanish Shrestha <avanissh@gmail.com>
2025-03-06 13:03:13 +0545

    update: minor refactors

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="pulse",  # Name of your package
-    version="0.2",
+    version="0.3",
     packages=find_packages(),
     install_requires=["click"],  # Add any other dependencies you may have
     entry_points={
