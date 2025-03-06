77363c333959defadd62ff1f0cbd881a36063b26
Avanish Shrestha <avanissh@gmail.com>
2025-03-06 12:37:05 +0545

    update: cli name

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="pulse-cli",  # Name of your package
-    version="0.1",
+    name="pulse",  # Name of your package
+    version="0.2",
     packages=find_packages(),
     install_requires=["click"],  # Add any other dependencies you may have
     entry_points={
         "console_scripts": [
-            "pulse-cli = pulse.pulse:cli",  # CLI entry point
+            "pulse = pulse.pulse:cli",  # CLI entry point
         ]
     },
     classifiers=[
