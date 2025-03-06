40ceabb0fe933ad344eb4e2f421c6b3ce43109b0
Avanish Shrestha <avanissh@gmail.com>
2025-03-06 13:13:06 +0545

    update: minor refactors

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="pulse",  # Name of your package
-    version="0.3",
+    version="0.4.0",
     packages=find_packages(),
     install_requires=["click", "tabulate", "python-dotenv"],  # Add any other dependencies you may have
     entry_points={
